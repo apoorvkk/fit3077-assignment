fd2e3158478c2767c1f88703e298c417c5594b6d
Apoorv Kansal <akan57@student.monash.edu>
2019-04-28 11:53:37 +1000

    Defined a constant for client id that will be used for socket registration.

---
+++ FrontEnd/DashboardMonitor/src/main/java/edu/monash/it/fit3077/vjak/Constant.java
@@ -0,0 +1,7 @@
+package edu.monash.it.fit3077.vjak;
+
+import java.util.UUID;
+
+public class Constant {
+    public static final String clientId = UUID.randomUUID().toString() + System.currentTimeMillis();
+}
