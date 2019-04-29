03d113adf1c7cfa73204457e2d2afeae93e88b52
Apoorv Kansal <akan57@student.monash.edu>
2019-04-29 21:02:45 +1000

    Setup spring app and web socket config.

--- BackEnd/server/src/main/java/edu/monash/it/fit3077/vjak/Application.java
+++ BackEnd/server/src/main/java/edu/monash/it/fit3077/vjak/Application.java
@@ -1,7 +1,12 @@
 package edu.monash.it.fit3077.vjak;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
+        SpringApplication.run(edu.monash.it.fit3077.vjak.Application.class, args);
     }
 }
