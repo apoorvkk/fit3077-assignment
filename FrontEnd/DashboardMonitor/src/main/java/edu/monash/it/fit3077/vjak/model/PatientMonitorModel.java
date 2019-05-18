38689fcd703b477d85c75a09625fc8244ec90394
Apoorv Kansal <akan57@student.monash.edu>
2019-05-19 01:28:31 +1000

    Refactor patient monitor model to use global tracking of measurements

--- FrontEnd/DashboardMonitor/src/main/java/edu/monash/it/fit3077/vjak/model/PatientMonitorModel.java
+++ FrontEnd/DashboardMonitor/src/main/java/edu/monash/it/fit3077/vjak/model/PatientMonitorModel.java
@@ -1,61 +1,71 @@
 package edu.monash.it.fit3077.vjak.model;
 
+import edu.monash.it.fit3077.vjak.Constant;
 import edu.monash.it.fit3077.vjak.backendmonitor.HealthMeasurementListener;
 import edu.monash.it.fit3077.vjak.backendmonitor.MeasurementEventModel;
-import edu.monash.it.fit3077.vjak.model.health.HealthMeasurementModel;
+import edu.monash.it.fit3077.vjak.model.health.HealthMeasurementCreator;
+import edu.monash.it.fit3077.vjak.model.health.AbstractHealthMeasurementModel;
+import edu.monash.it.fit3077.vjak.observer.Subject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /*
 This class is responsible for managing all tracked measurements on a patient. For a given patient, you can monitor
 various measurements such as cholesterol. You can start and stop tracking these health measurements too.
  */
-public class PatientMonitorModel implements PatientMonitorModelInterface {
+public class PatientMonitorModel extends Subject implements PatientMonitorModelInterface {
     private final PatientModelInterface patient;
-    private final ArrayList<HealthMeasurementModel> healthMeasurements;
-    private final HealthMeasurementCreator healtMeasurementCreator;
+    private final ArrayList<AbstractHealthMeasurementModel> healthMeasurements;
     private HealthMeasurementListener healthMeasurementListener;
     private Boolean isBeingMonitored;
 
     PatientMonitorModel(PatientModelInterface p, HealthMeasurementListener hl) {
         this.isBeingMonitored = false;
-        this.observeHealthMeasurements(hl);
+        this.healthMeasurementListener = hl;
 
         this.patient = p;
         this.healthMeasurements = new ArrayList<>();
-        this.healtMeasurementCreator = new HealthMeasurementCreator();
-    }
-
-    private void observeHealthMeasurements(HealthMeasurementListener hl) {
-        this.healthMeasurementListener = hl;
     }
 
     private void cleanUp() {
         // Make sure to detach from the socket listener and remove all measurements.
         this.healthMeasurementListener.detach(this);
-        this.healthMeasurements.forEach(HealthMeasurementModel::cleanUp);
+        this.healthMeasurements.forEach(AbstractHealthMeasurementModel::cleanUp);
         this.healthMeasurements.clear();
         this.isBeingMonitored = false;
     }
 
-    public void trackCholesterol() {
-        HealthMeasurementModel hm = this.healtMeasurementCreator.trackCholesterol(this.patient.getId());
+    private void registerHealthMeasurement(AbstractHealthMeasurementModel hm) {
         this.healthMeasurements.add(hm);
+        this.notifyObservers();
     }
 
-    public void trackSystolicBloodPressure() {
-        HealthMeasurementModel hm = this.healtMeasurementCreator.trackSystolicBloodPressure(this.patient.getId());
-        this.healthMeasurements.add(hm);
+    private AbstractHealthMeasurementModel getByMeasurementType(String measurementType) {
+        int i = 0;
+        while (i < this.healthMeasurements.size()) {
+            AbstractHealthMeasurementModel curr = this.healthMeasurements.get(i);
+            if (curr.getMeasurementType().equals(measurementType)) {
+                return curr;
+
+            }
+            i++;
+        }
+        return null;
     }
 
-    public void trackDiastolicBloodPressure() {
-        HealthMeasurementModel hm = this.healtMeasurementCreator.trackDiastolicBloodPressure(this.patient.getId());
-        this.healthMeasurements.add(hm);
+    public void removeHealthMeasurement(String measurementType) {
+        AbstractHealthMeasurementModel hm = this.getByMeasurementType(measurementType);
+        if (hm != null) {
+            hm.cleanUp();
+            this.healthMeasurements.remove(hm);
+            this.notifyObservers();
+        }
     }
 
-    public void trackTobaccoUse() {
-        HealthMeasurementModel hm = this.healtMeasurementCreator.trackTobaccoUse(this.patient.getId());
-        this.healthMeasurements.add(hm);
+    public void trackMeasurement(String measurementType) {
+        AbstractHealthMeasurementModel hm = HealthMeasurementCreator.trackHealthMeasurement(this.patient.getId(), measurementType);
+        this.registerHealthMeasurement(hm);
     }
 
     public void startMonitoring() {
@@ -75,19 +85,27 @@ public class PatientMonitorModel implements PatientMonitorModelInterface {
         return this.patient;
     }
 
-    public ArrayList<HealthMeasurementModel> getHealthMeasurements() {
-        return this.healthMeasurements;
+    public ArrayList<AbstractHealthMeasurementModel> getHealthMeasurements() {
+        ArrayList<AbstractHealthMeasurementModel> orderedHealthMeasurements = new ArrayList<>();
+
+        Constant.monitorOrder.forEach(type -> {
+            AbstractHealthMeasurementModel hm = this.getByMeasurementType(type);
+            if (hm != null) {
+                orderedHealthMeasurements.add(hm);
+            }
+        });
+
+        return orderedHealthMeasurements;
     }
 
     public void update() {
         // Listen into socket event and update the relevant measurement models.
         MeasurementEventModel me = this.healthMeasurementListener.getDataReceived();
-
         if (me.getPatient().equals(this.patient.getId())) {
             this.healthMeasurements
                 .forEach(healthMeasurement -> {
                     String measurementType = me.getType();
-                    if (measurementType.equals(healthMeasurement.toString())) {
+                    if (measurementType.equals(healthMeasurement.getMeasurementType())) {
                         healthMeasurement.setHealthMeasurementValue(me);
                     }
                 });
